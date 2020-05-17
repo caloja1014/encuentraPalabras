881fe324af5f3466d881709666952a5015aedc58
caloja1014 <caloja@espol.edu.ec>
2020-05-17 18:32:08 -0500

    Agregar Idiomas

--- src/encuentrapalabras/EncuentraPalabras.java
+++ src/encuentrapalabras/EncuentraPalabras.java
@@ -5,6 +5,9 @@
  */
 package encuentrapalabras;
 
+import interfaces.InterfazIdioma;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -20,35 +23,27 @@ import modelo.LecturaIdioma;
  * @author CLOJA
  */
 public class EncuentraPalabras extends Application {
+    Toolkit t = Toolkit.getDefaultToolkit();
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
     final static Stage stage= new Stage();
+    
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        InterfazIdioma i=new InterfazIdioma();
+
         
-        Scene scene = new Scene(root, 300, 250);
+        Scene scene = new Scene(i.getRoot(), screenSize.width*0.7, screenSize.height*0.7);
         
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        stage.setTitle("Hello World!");
+        stage.setScene(scene);
+        stage.show();
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        LecturaIdioma l= new LecturaIdioma("italiano");
-        System.out.println(l.getIdiomaSet());
-        System.out.println(Idiomas.italiano.toString());
         launch(args);
         
     }
--- src/interfaces/InterfazIdioma.java
+++ src/interfaces/InterfazIdioma.java
@@ -5,9 +5,13 @@
  */
 package interfaces;
 
+import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
-
+import modelo.Idiomas;
 /**
  *
  * @author CLOJA
@@ -15,6 +19,28 @@ import javafx.scene.layout.VBox;
 public class InterfazIdioma  {
     private final VBox root= new VBox();
     private final Label titulo= new Label("Escoge el Idioma");
+
+    public InterfazIdioma() {
+        organizarElementos();
+    }
     
+    private void organizarElementos(){
+        root.setSpacing(10);
+        root.autosize();
+        root.setAlignment(Pos.CENTER);
+        final ObservableList<Node> l= root.getChildren();
+        l.add(titulo);
+        Idiomas[] idi=Idiomas.values();
+        for (int i=0;i<idi.length;i++){
+            Button btnIdioma= new Button(idi[i].toString().toUpperCase());
+            l.add(btnIdioma);
+        }
+        
+        
+    }
+
+    public VBox getRoot() {
+        return root;
+    }
     
 }
